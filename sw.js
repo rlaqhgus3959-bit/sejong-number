e21b497534ec2f6b1b08b7a1c630f50b5e4c890c
rlaqhgus3959-bit <rlaqhgus3959@gmail.com>
2026-02-12 13:28:51 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+﻿const CACHE_NAME = 'police-dms-v1';
+const urlsToCache = ['./index.html', './manifest.json'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
