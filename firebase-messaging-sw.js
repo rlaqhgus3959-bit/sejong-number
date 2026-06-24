6329bd95b171fb8dc7f54723372e1339ec51a557
rlaqhgus3959-bit <rlaqhgus3959@gmail.com>
2026-06-24 17:28:27 +0900

    firebase-messaging-sw.js 업데이트

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,27 +1,26 @@
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
-// 🚨 주의: 아래 firebaseConfig 안의 내용은 선생님의 파이어베이스 프로젝트 설정값으로 바꿔주셔야 합니다!
+// 🚨 파이어베이스 웹 앱(shut down) 화면에 나온 설정값을 아래에 똑같이 넣어주세요!
 const firebaseConfig = {
-  apiKey: "AIzaSyDJw2-fkhyX8ioYlKrv5ixNiCPi2kirY8k",
+  apiKey: "선생님의_API_KEY",
   authDomain: "shut-down-80255.firebaseapp.com",
   databaseURL: "https://shut-down-80255-default-rtdb.firebaseio.com",
   projectId: "shut-down-80255",
-  storageBucket: "shut-down-80255.firebasestorage.app",
-  messagingSenderId: "620282815079",
-  appId: "1:620282815079:web:bd561b5a00483e94fdaac2",
-  measurementId: "G-XKW8CM4W06"
+  storageBucket: "shut-down-80255.appspot.com",
+  messagingSenderId: "선생님의_SENDER_ID",
+  appId: "선생님의_APP_ID"
 };
 
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-// 백그라운드에서 알림이 왔을 때 화면에 띄워주는 역할
+// 상황실 브라우저가 내려가 있을 때 알림을 띄워주는 역할
 messaging.onBackgroundMessage(function(payload) {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/firebase-logo.png' // 원하시는 경찰 마크나 알림 아이콘 이미지 경로로 변경 가능합니다.
+    icon: '/firebase-logo.png' // 추후 경찰 마크 이미지 등으로 변경 가능합니다.
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
