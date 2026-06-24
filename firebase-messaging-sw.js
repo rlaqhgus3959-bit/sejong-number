3528b80f535c0681968e8c935ff2467ca725944e
rlaqhgus3959-bit <rlaqhgus3959@gmail.com>
2026-06-24 17:31:07 +0900

    firebase-messaging-sw.js 업데이트

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,26 +1,28 @@
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
-// 🚨 파이어베이스 웹 앱(shut down) 화면에 나온 설정값을 아래에 똑같이 넣어주세요!
 const firebaseConfig = {
-  apiKey: "선생님의_API_KEY",
+  apiKey: "AIzaSyDJw2-fkhyX8ioYlKrv5ixNiCPi2kirY8k",
   authDomain: "shut-down-80255.firebaseapp.com",
   databaseURL: "https://shut-down-80255-default-rtdb.firebaseio.com",
   projectId: "shut-down-80255",
-  storageBucket: "shut-down-80255.appspot.com",
-  messagingSenderId: "선생님의_SENDER_ID",
-  appId: "선생님의_APP_ID"
+  storageBucket: "shut-down-80255.firebasestorage.app",
+  messagingSenderId: "620282815079",
+  appId: "1:620282815079:web:bd561b5a00483e94fdaac2",
+  measurementId: "G-XKW8CM4W06"
 };
 
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-// 상황실 브라우저가 내려가 있을 때 알림을 띄워주는 역할
+// 백그라운드 메시지 수신 시 알림 처리
 messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/firebase-logo.png' // 추후 경찰 마크 이미지 등으로 변경 가능합니다.
+    icon: '/firebase-logo.png' // 필요시 경찰서 로고 경로로 수정하세요.
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
